7497d63128693d2159cc36fe8374d28cf034bd76
Armelle <armelle.lecerf@orange.fr>
2020-03-31 22:34:56 +0200

    ajout de la structure Question

---
+++ Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  OpenQuizz2
+//
+//  Created by macmini-Armelle on 31/03/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import Foundation
+struct Question {
+    var title = ""
+    var isCorrect = false
+}
